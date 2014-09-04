6e259b0b3dc61f6a1a20b67123a846cce9bb026d
Nicholas Klunder <nklunder@gmail.com>
2014-09-04 09:55:50 -0700

    seperate CSS and JS from HTML

---
+++ nicholas-klunder-jay-sweet/scripts/app.js
@@ -0,0 +1,19 @@
+var fileInput = document.getElementById("fileInput");
+var secretMessage = document.getElementById("secretMessage");
+
+fileInput.addEventListener('change', function(e) {
+  var userImage = fileInput.files[0];
+
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+
+    var dataURI = reader.result;
+    var styleUpdate = "background: url(" + dataURI + "); -webkit-background-clip: text; -webkit-text-fill-color: transparent; text-shadow: 1px 2px 5px rgba(0,0,0,0.6);";
+
+    secretMessage.setAttribute("style", styleUpdate);
+  };
+
+  reader.readAsDataURL(userImage);
+
+});
