60236e41dff3e8f8e15cfb0aee5ca93b65707cc0
Monica <monicaedavidson@gmail.com>
2014-10-30 13:38:42 -0700

    Create file-reader.js
    first commit

---
+++ FileReader/file-reader.js
@@ -0,0 +1,16 @@
+var fileInput = document.getElementById('fileInput');
+var fileDisplayArea = document.getElementById('fileDisplayArea');
+
+fileInput.addEventListener('change', function() {
+  var file = fileInput.files[0];
+  var textType = /text.*/;
+  if (file.type.match(textType)) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      fileDisplayArea.innerText = reader.result;
+    }
+    reader.readAsText(file);
+  } else {
+      fileDisplayArea.innerText = "File not supported!";
+  }
+})
